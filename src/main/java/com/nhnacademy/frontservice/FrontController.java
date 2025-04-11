ad9eb331330cadfd58d8d45d82dd5c456efbe5af
inho <dusen0528@naver.com>
2025-04-11 16:03:18 +0900

    feature : 랜딩페이지 병합

--- src/main/java/com/nhnacademy/frontservice/FrontController.java
+++ src/main/java/com/nhnacademy/frontservice/FrontController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FrontController {
-    @GetMapping(value = {"/index","/"})
+    @GetMapping(value = {"/index"})
     public String index(){
         return "index/index";
     }
@@ -14,6 +14,10 @@ public class FrontController {
     public String showLoginPage() {
         return "index/login";
     }
+
+    @GetMapping("/")
+    public String randing(){return "index/landing";}
+
 
     @GetMapping(value = {"/charts"})
     public String charts() { return "index/charts";}
