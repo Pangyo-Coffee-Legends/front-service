32b772292939f5853510a5a6751be0c8953ff36e
ur <dorosi150@gmail.com>
2025-04-16 16:24:29 +0900

    fix : Entity 삭제 - closes #13

--- src/main/java/com/nhnacademy/frontservice/service/MemberService.java
+++ src/main/java/com/nhnacademy/frontservice/service/MemberService.java
@@ -1,22 +1,7 @@
 package com.nhnacademy.frontservice.service;
 
-import com.nhnacademy.frontservice.adaptor.GatewayAdaptor;
-import com.nhnacademy.frontservice.model.dto.MemberResponse;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
+import com.nhnacademy.frontservice.dto.MemberResponse;
 
-@Service
-@RequiredArgsConstructor
-public class MemberService {
-    private final GatewayAdaptor gatewayAdaptor;
-
-    public MemberResponse findBymbEmail(String mbEmail) {
-        MemberResponse member = gatewayAdaptor.findBymbEmail(mbEmail);
-        if (member == null) {
-            throw new UsernameNotFoundException("Member not found.");
-        }
-
-        return member;
-    }
+public interface MemberService {
+    MemberResponse getMbEmail(String mbEmail);
 }
