2e1fc0b3dbda3fd1d66fd5ca2fcb4f87b9c4648b
ur <dorosi150@gmail.com>
2025-04-17 08:52:17 +0900

    remove : WebConfig 삭제 - closes #11
    restTemplate 삭제

--- src/main/java/com/nhnacademy/frontservice/common/handler/LoginSuccessHandler.java
+++ src/main/java/com/nhnacademy/frontservice/common/handler/LoginSuccessHandler.java
@@ -12,7 +12,6 @@ import org.springframework.security.oauth2.client.authentication.OAuth2Authentic
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -21,7 +20,6 @@ import java.util.Map;
 @RequiredArgsConstructor
 @Component
 public class LoginSuccessHandler implements AuthenticationSuccessHandler {
-    private final RestTemplate restTemplate;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
@@ -31,16 +29,10 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
             String email = oidcUser.getEmail();
             String name = oidcUser.getFullName();
 
-
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_JSON);
-
             Map<String, Object> payload = new HashMap<>();
             payload.put("email", email);
             payload.put("name", name);
 
-            HttpEntity<Map<String, Object>> entity = new HttpEntity<>(payload, headers);
-//            restTemplate.postForEntity("http://localhost:10244/api/users/login", entity, Void.class); // oauth register
 
             response.sendRedirect("/index");
         }
