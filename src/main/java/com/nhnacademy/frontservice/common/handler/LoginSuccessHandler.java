f646d82de7826484b12b7a944146d57672908be7
ur <dorosi150@gmail.com>
2025-04-17 09:34:06 +0900

    fix : scope 추가 - closes #11

--- src/main/java/com/nhnacademy/frontservice/common/handler/LoginSuccessHandler.java
+++ src/main/java/com/nhnacademy/frontservice/common/handler/LoginSuccessHandler.java
@@ -4,11 +4,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -24,17 +20,14 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
 
-        if(authentication instanceof OAuth2AuthenticationToken){
-            OidcUser oidcUser = (OidcUser) authentication.getPrincipal();
-            String email = oidcUser.getEmail();
-            String name = oidcUser.getFullName();
+        OidcUser oidcUser = (OidcUser) authentication.getPrincipal();
+        String email = oidcUser.getEmail();
+        String name = oidcUser.getFullName();
 
-            Map<String, Object> payload = new HashMap<>();
-            payload.put("email", email);
-            payload.put("name", name);
+        Map<String, Object> payload = new HashMap<>();
+        payload.put("email", email);
+        payload.put("name", name);
 
-
-            response.sendRedirect("/index");
-        }
+        response.sendRedirect("/index");
     }
 }
