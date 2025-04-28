d1e113303aa2ebdab41e8548650fc4de58e5b174
Migong0311 <kimms000311@gmail.com>
2025-04-28 15:35:17 +0900

    fix : member 리스트 받기 위한 경로 수정

--- src/main/java/com/nhnacademy/frontservice/service/MemberService.java
+++ src/main/java/com/nhnacademy/frontservice/service/MemberService.java
@@ -1,9 +1,8 @@
 package com.nhnacademy.frontservice.service;
 
-import com.nhnacademy.frontservice.dto.MemberRegisterRequest;
-import com.nhnacademy.frontservice.dto.MemberResponse;
-import com.nhnacademy.frontservice.dto.MemberUpdatePasswordRequest;
-import com.nhnacademy.frontservice.dto.MemberUpdateRequest;
+import com.nhnacademy.frontservice.dto.*;
+
+import java.util.List;
 
 public interface MemberService {
     MemberResponse getMbEmail(String mbEmail);
@@ -15,4 +14,6 @@ public interface MemberService {
     MemberResponse deleteMember(Long mbNo);
 
     MemberResponse updatePassword(Long mbNo, MemberUpdatePasswordRequest registerRequest);
+
+    List<MemberInfoResponse> getMemberInfoList();
 }
