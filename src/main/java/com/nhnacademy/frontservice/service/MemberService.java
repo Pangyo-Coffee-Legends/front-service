065399ef262b9da0dbc116d21f93c3db7b9dc52e
rudduddl <kim5472678@gmail.com>
2025-05-07 17:26:51 +0900

    attendace controller, member controller 불필요한 메소드 삭제

--- src/main/java/com/nhnacademy/frontservice/service/MemberService.java
+++ src/main/java/com/nhnacademy/frontservice/service/MemberService.java
@@ -9,11 +9,4 @@ public interface MemberService {
 
     MemberResponse register(MemberRegisterRequest registerRequest);
 
-    MemberResponse updateMember(Long mbNo, MemberUpdateRequest registerRequest);
-
-    MemberResponse deleteMember(Long mbNo);
-
-    MemberResponse updatePassword(Long mbNo, MemberUpdatePasswordRequest registerRequest);
-
-    List<MemberInfoResponse> getMemberInfoList();
 }
