708450a5abc1ae14abc5c5363ee405ced96fc745
Migong0311 <kimms000311@gmail.com>
2025-05-07 17:32:27 +0900

    fix : fetch요청에 따른 불필요한 로직 제거

--- src/main/java/com/nhnacademy/frontservice/service/MemberService.java
+++ src/main/java/com/nhnacademy/frontservice/service/MemberService.java
@@ -8,12 +8,4 @@ public interface MemberService {
     MemberResponse getMbEmail(String mbEmail);
 
     MemberResponse register(MemberRegisterRequest registerRequest);
-
-    MemberResponse updateMember(Long mbNo, MemberUpdateRequest registerRequest);
-
-    MemberResponse deleteMember(Long mbNo);
-
-    MemberResponse updatePassword(Long mbNo, MemberUpdatePasswordRequest registerRequest);
-
-    List<MemberInfoResponse> getMemberInfoList();
 }
