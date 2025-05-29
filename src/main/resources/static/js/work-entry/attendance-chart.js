971b4687733b9335f0e59f7dca4adab7d08bfdcd
rudduddl <kim5472678@gmail.com>
2025-05-29 16:28:31 +0900

    fix : 근태 데이터 조회 경로 수정

--- src/main/resources/static/js/work-entry/attendance-chart.js
+++ src/main/resources/static/js/work-entry/attendance-chart.js
@@ -337,7 +337,7 @@ document.addEventListener('DOMContentLoaded', function () {
             return;
         }
 
-        fetchWithAuth(`http://localhost:10251/api/v1/attendances/summary/recent/${currentMemberNo}`)
+        fetchWithAuth(`http://localhost:10251/api/v1/attendances/${currentMemberNo}/summary/recent`)
             .then(res => res.json())
             .then(json => {
                 currentData = json.content.filter(it => it.year === y && it.monthValue === m);
